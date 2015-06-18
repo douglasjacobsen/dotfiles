8b25bcc1ef6b5154bc2de760204f846a80e5b53d
Douglas Jacobsen <jacobsen.douglas@gmail.com>
2015-06-18 12:42:18 -0600

    Adding history search for all shells

--- scripts/setup_aliases.sh
+++ scripts/setup_aliases.sh
@@ -48,20 +48,18 @@ alias reset_unity='stop unity-panel-service; start unity-panel-service'
 #########################################################################################
 #					Set Commands for Terminal History Search							#
 #########################################################################################
-if [ "${SHELL##*/}" == "bash" ]; then
-	set filec
-	set history = 100 
-	set savehist = 100 
-	
-	bind -r "\e[A"
-	bind -r "\e[B"
-	bind -r "\e[1;5C"
-	bind -r "\e[1;5D"
-	bind '"\e[A": history-search-backward'
-	bind '"\e[B": history-search-forward'
-	bind '"\e[1;5C": forward-word'
-	bind '"\e[1;5D": backward-word' 
-fi
+set filec
+set history = 100 
+set savehist = 100 
+
+bind -r "\e[A"
+bind -r "\e[B"
+bind -r "\e[1;5C"
+bind -r "\e[1;5D"
+bind '"\e[A": history-search-backward'
+bind '"\e[B": history-search-forward'
+bind '"\e[1;5C": forward-word'
+bind '"\e[1;5D": backward-word' 
 
 ON_UBUNTU=`uname -a | grep "Ubuntu"`
 
